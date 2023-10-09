d0d04fd38f931bd2702d8effc72e7290f772b9e4
Alfonso Garcia <alfonso.garcia2324@gmail.com>
2023-10-09 11:31:48 -0700

    init

---
+++ custom/mappings.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = { "<cmd>DapToggleBreakpoint<CR>", "Add breakpoint at line" },
+    ["<leader>dr"] = { "<cmd>DapContinue<CR>", "Start or continue the debugger" },
+  },
+}
+
+M.general = {
+  n = {
+    ["<C-h>"] = { "<cmd>TmuxNavigateLeft<CR>", "window left" },
+    ["<C-l>"] = { "<cmd>TmuxNavigateRight<CR>", "window right" },
+    ["<C-j>"] = { "<cmd>TmuxNavigateDown<CR>", "window down" },
+    ["<C-k>"] = { "<cmd>TmuxNavigateUp<CR>", "window up" },
+  },
+  i = {
+    ["jk"] = { "<ESC>", "escape insert mode", opts = { nowait = true } }
+  }
+}
+
+return M
---
+++ custom/plugins.lua
@@ -0,0 +1,73 @@
+local plugins = {
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "clang-format",
+        "codelldb",
+        "typescript-language-server",
+        "eslint-lsp",
+        "prettier",
+        "js-debug-adapter"
+      },
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function ()
+      require("custom.configs.dap")
+      require("core.utils").load_mappings("dap")
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+    },
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function ()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function ()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function ()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function ()
+        dapui.close()
+      end
+    end
+  }
+}
+
+return plugins
